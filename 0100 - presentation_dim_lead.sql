8987efd38e131664f7d5778c67ef7a36abbc251e
Manuel Müller <manuelmuller@Manuels-MacBook-Pro.local>
2019-08-29 14:33:10 +0200

    check

--- 0100 - presentation_dim_lead.sql
+++ 0100 - presentation_dim_lead.sql
@@ -84,7 +84,7 @@ WITH src AS (
                                       -- logic duplicated from datamart.fact_partner_status_monthly
                                       WITH max_weekly_date AS (
 
-Case sensitivty is a cool thing. Or not.
+Case sensitivty is a cool thing. Or not. Freaking githubs redeliver does not work.
 
 ALTER TABLE datamart.presentation_dim_lead
   OWNER TO dbmaster;
