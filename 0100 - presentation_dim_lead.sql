55ea10d49bdace0f70d830710cabdb42860d7357
Manuel Müller <manuelmuller@Manuels-MacBook-Pro.local>
2019-08-29 14:27:09 +0200

    case insensitivity

--- 0100 - presentation_dim_lead.sql
+++ 0100 - presentation_dim_lead.sql
@@ -83,7 +83,8 @@ SUM(
 WITH src AS (
                                       -- logic duplicated from datamart.fact_partner_status_monthly
                                       WITH max_weekly_date AS (
-                                        
+
+Case sensitivty is a cool thing.
 
 ALTER TABLE datamart.presentation_dim_lead
   OWNER TO dbmaster;
