7d05583223206ae2684426f21a7773569136eb69
Manuel Müller <manuelmuller@Manuels-MacBook-Pro.local>
2019-08-29 14:38:43 +0200

    das

--- 0100 - presentation_dim_lead.sql
+++ 0100 - presentation_dim_lead.sql
@@ -85,7 +85,7 @@ WITH src AS (
                                       WITH max_weekly_date AS (
 
 Case sensitivty is a cool thing. Or not. Freaking githubs redeliver does not work.
-
+Small update
 ALTER TABLE datamart.presentation_dim_lead
   OWNER TO dbmaster;
 --rollback DROP VIEW datamart.presentation_dim_lead;
