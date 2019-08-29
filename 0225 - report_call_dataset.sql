d7d9df8ca76ea9b33853d230981721e00046a650
Manuel Müller <manuelmuller@Manuels-MacBook-Pro.local>
2019-08-29 14:17:33 +0200

    removing bad words

--- 0225 - report_call_dataset.sql
+++ 0225 - report_call_dataset.sql
@@ -6,9 +6,6 @@
 -- Dimension Grain: Call ID, Date (Day)
 --
 -- Description: Contains call data which is not true
--- This is shit. Fucking piece does not work.
--- another shit comment fuck wtf WTF
--- interesting wtf shit
 --
 -- ========================
 -- |- Editing Guidelines -|
